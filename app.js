274db4121c588e6b6493bac1f605b129c2fe672e
oleskiy OS <12lie.to.me04@gmail.com>
2020-06-27 20:59:27 +0200

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname));
 
 app.get("/", function(req, res) {
-  res.sendFile("index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT, process.env.IP);
+app.listen(3001, function () {
+  console.log("Your server has been started...")
+});
